9e5dcf0a8e89417d1800aeb2668e1e5da79c2e10
Kimgeunsoo <geunsoo1106@gmail.com>
2022-09-21 17:49:05 +0900

    간단히 추억살려서 네이버 a태그 리액트로ㄱㄱ

---
+++ react-study-chobo/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  const name = "Tom";
+  const naver = {
+    name: "네이버",
+    url: "http://naver.com",
+  };
+  return (
+    <div className="App">
+      <h1
+        style={{
+          color: "red",
+          backgroundColor: "green",
+        }}
+      >
+        Welcome, {name}, <p>{2 + 3}</p>
+      </h1>
+      <a href={naver.url}>{naver.name}</a>
+    </div>
+  );
+}
+
+export default App;
